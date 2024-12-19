de1e3b1cb6ccde2fd1b4f5da43e36d26febbccc3
sohamsahu85 <soham.aditya24@gmail.com>
2024-12-19 22:46:18 +0530

    Add files via upload

--- question 15.c
+++ question 15.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+#include <string.h>
 
+
+void compress_string(char* s, int index, int count, char* result) {
+
+    if (s[index] == '\0') {
+        return;
+    }
+
+
+    if (s[index] == s[index + 1]) {
+
+        compress_string(s, index + 1, count + 1, result);
+    } else {
+
+        char count_str[10];
+        sprintf(count_str, "%d", count);
+        strcat(result, (char[]){s[index], '\0'});
+        strcat(result, count_str);
+
+
+        compress_string(s, index + 1, 1, result);
+    }
+}
+
+int main() {
+    char input_string[] = "aaabbc";
+    char result[100] = "";
+
+    compress_string(input_string, 0, 1, result);
+
+    printf("Compressed string: %s\n", result);
+
+    return 0;
+}
